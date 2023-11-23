a851e57dae980e08a1f840ebdc2ca656c11d288f
Yt1m <144239923+Yt1m@users.noreply.github.com>
2023-11-23 12:49:05 +0200

    Add files via upload

---
+++ 4-1.cs
@@ -0,0 +1,23 @@
+﻿double[] array = new double[14];
+Console.WriteLine("Введите 14 элеменов массива:");
+for (int i = 0; i < 14; i++)
+{
+    array[i] = double.Parse(Console.ReadLine());
+}
+int halfLength = array.Length / 2;
+int swapsCount = SwapElements(array, halfLength);
+Console.WriteLine($"Измененный массив:{string.Join(", ", array)}");
+Console.WriteLine($"Количество перестановок {swapsCount}");
+Console.ReadLine();
+static int SwapElements(double[] array, int halfLength)
+{
+    int swapsCount = 0;
+    for (int i = 0; i < halfLength; i++)
+    {
+        double temp = array[i];
+        array[i] = array[i + halfLength];
+        array[i + halfLength] = temp;
+        swapsCount++;
+    }
+    return swapsCount;
+}
